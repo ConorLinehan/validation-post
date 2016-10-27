4479dec11efea73e6fdb325d6ff421dc4b221b4c
Conor Linehan <conorlinehan9@gmail.com>
2016-10-27 12:09:07 +0100

    remove validation logic from form

--- app/components/signup-form.js
+++ app/components/signup-form.js
@@ -1,35 +1,10 @@
 import Ember from 'ember';
-import {
-  validator,
-  buildValidations
-} from 'ember-cp-validations';
 
 const {
   computed
 } = Ember;
 
-const Validations = buildValidations({
-  name: [
-    validator('presence', true, {
-      message: 'Name must be present'
-    }),
-    validator('format', {
-      regex: /^[a-zA-Z  ']+$/,
-      message: 'Name can only contain letters'
-    })
-  ],
-  address: validator('length', {
-    is: 2,
-    message: 'You can only select 2 addresses'
-  }),
-  palindrome: validator('palindrome', {
-    message: 'Must be a valid palindrome'
-  }),
-  email: validator('unique-email'),
-  media: validator('is-media')
-});
-
-export default Ember.Component.extend(Validations, {
+export default Ember.Component.extend({
   name: computed.alias('user.name'),
   addresses: computed.alias('user.addresses'),
   palindrome: computed.alias('user.favoritePalindrome'),
